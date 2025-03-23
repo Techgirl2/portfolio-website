c9767615654b7fcd96e3f0befccbb71244297ce9
Angelina Cruz <angelinacruz465@gmail.com>
2025-03-23 16:42:06 -0700

    background added and reorganized files

---
+++ src/LandingPage/Landing.tsx
@@ -0,0 +1,9 @@
+import PageHeader from "./pageheader";
+
+export default function LandingPage() {
+  return (
+    <div className="relative w-full h-screen bg-cover bg-center bg-no-repeat bg-[url('/Background.svg')]">
+      <PageHeader />
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import "./index.css";
-import PageHeader from "./pageheader";
+import LandingPage from "./LandingPage/Landing";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PageHeader />
+    <LandingPage />
   </StrictMode>,
 )
