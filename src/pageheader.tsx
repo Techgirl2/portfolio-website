7df3ae77a5ed03f15f183e4118b3b282d90b6725
Angelina Cruz <angelinacruz465@gmail.com>
2025-03-23 14:37:00 -0700

    trying to deploy

--- src/pageheader.tsx
+++ src/pageheader.tsx
@@ -10,7 +10,7 @@ const PageHeader: React.FC = () => {
     <header className="bg-[#18424E] text-white flex justify-between items-center px-6 py-4 shadow-md border-b-4 border-[#D9D9D9]/90 relative">
       {/* Directly using the logo */}
       <button>
-        <img src="/Logo.svg" alt="AC Logo" className="w-10 h-10" />
+        <img src="/Logo.svg" alt="AC Logo" className="w-10 h-6" />
       </button>
 
       {/* Desktop Menu */}
