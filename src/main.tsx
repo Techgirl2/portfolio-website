935332702ec356d1637c1a620e33aa618e12fc5d
Angelina Cruz <angelinacruz465@gmail.com>
2025-03-23 15:27:49 -0700

    trying to deploy again

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import PageHeader from './pageheader.tsx'
-//import About from './about.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import "./index.css";
+import PageHeader from "./pageheader";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
